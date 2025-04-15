cab9e0605f94805ec8d8e4cfa934139f1cc9ab7d
Javier Brausin <jbrausin@gmail.com>
2025-04-15 09:44:43 -0500

    Se agregaron varibales de color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#a855f7',    // morado
+        accent: '#d8f277',     // verde
+        dark: '#111827',       // tono más oscuro para fondo sidebar
+        light: '#faf9f5',      // tono de fondo pantalla principal
+        card: '#ffffff',       // blanco para fondo de las tarjetas
+      },
+    },
   },
   plugins: [],
 }
