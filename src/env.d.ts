98d73a037057e31a6a255ae8611c62c00ef77162
Javier Brausin <jbrausin@gmail.com>
2025-04-15 10:22:53 -0500

    Se ajustó la jerarquía y se agregó la opción de idiomas

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,24 +1,33 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
+interface Language {
+  id: string;
+  name: string;
+}
+
 interface StudyGroup {
-    id: string;
-    name: string;
-  }
-  
-  interface StudyCard {
-    id: string;
-    english: string;
-    translation: string;
-    learned: boolean;
-  }
-  
-  // Extend Window interface to include our custom functions
-  interface Window {
-    openCreateGroupModal: () => void;
-    openEditGroupModal: (groupId: string, groupName: string) => void;
-    openAddCardModal: (groupId: string) => void;
-    openDeleteGroupModal: (groupId: string) => void;
-    openEditCardModal: (cardId: string, english: string, translation: string, groupId?: string) => void;
-    openDeleteCardModal: (cardId: string, groupId?: string) => void;
-  }
+  id: string;
+  name: string;
+  languageId: string;
+}
+
+interface StudyCard {
+  id: string;
+  english: string;
+  translation: string;
+  learned: boolean;
+}
+
+// Extend Window interface to include our custom functions
+interface Window {
+  openCreateLanguageModal: () => void;
+  openEditLanguageModal: (languageId: string, languageName: string) => void;
+  openDeleteLanguageModal: (languageId: string) => void;
+  openCreateGroupModal: (languageId: string) => void;
+  openEditGroupModal: (groupId: string, groupName: string, languageId: string) => void;
+  openAddCardModal: (groupId: string) => void;
+  openDeleteGroupModal: (groupId: string, languageId: string) => void;
+  openEditCardModal: (cardId: string, english: string, translation: string, groupId?: string) => void;
+  openDeleteCardModal: (cardId: string, groupId?: string) => void;
+}
